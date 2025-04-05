78faa0765e38219be6940cb4c2f55c64801609ca
Erick Souza <erick.erick669@gmail.com>
2025-04-05 15:38:32 -0300

    Atualiza a URL da API na configuração do Nuxt para incluir a porta 3003, mantendo a conexão segura via HTTPS.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      apiUrl: process.env.API_URL || "https://15.228.16.62",
+      apiUrl: 'https://15.228.16.62:3003',
     },
   },
   plugins: ["~/plugins/axios.ts"],
